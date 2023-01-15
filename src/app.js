2e41bb0c05a26dc5914a77dbaeb058984a884eca
sikai00 <sikai4111@gmail.com>
2023-01-15 08:29:36 +0800

    Fix CORS

--- src/app.js
+++ src/app.js
@@ -7,22 +7,28 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const whitelist = [
+var allowedOrigins = [
   "https://singa-script.vercel.app/",
   "https://singa-script.vercel.app/home",
 ];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.use(logger("dev"));
-app.use(cors(corsOptions));
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
