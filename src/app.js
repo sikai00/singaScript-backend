90e58a6c748370e139a1d219a05c226870027d72
sikai00 <sikai4111@gmail.com>
2023-01-15 08:35:09 +0800

    Fix CORS

--- src/app.js
+++ src/app.js
@@ -7,12 +7,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "https://singa-script.vercel.app/home",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(logger("dev"));
 
 app.get("/", function (req, res) {
